b16a3814822e2c526eac926081dfa2880d88d3ce
Liam Banga <liam205@outlook.com>
2018-05-16 20:56:06 +1000

    Enabled all CORS requests

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 
 const app = express();
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
