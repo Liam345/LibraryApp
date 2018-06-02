6565281108bd3a4f38f495b6ed1fd6961ea2d976
Liam Banga <liam205@outlook.com>
2018-06-03 08:50:43 +1000

    Changed config to serve static files from react app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,6 +11,10 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 require('./server/routes')(app);
 
